6c88a7aa8885877c38cfed0161aad80a8839639a
ean <eanplatter@gmail.com>
2016-03-03 10:11:27 -0600

    cat rc file

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 var shell = require('shelljs');
-
-exports.run = shell.exec('node node_modules/enclave/index.js');
-
+shell.cat('../../.enclaverc')
 shell.echo('Let the drive sequence being, hit it Pinback.');
 shell.exec('cd node_modules/enclave && npm start');
