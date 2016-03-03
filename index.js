46757ad6c349296f4aa2db5abf997a4479673efd
ean <eanplatter@gmail.com>
2016-03-03 11:53:32 -0600

    fix message

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var shell = require('shelljs');
 var setting = shell.grep('entry', '.enclaverc');
 exportedSetting = 'exports.entry = {' + setting + '}';
 exportedSetting.to('node_modules/enclave/settings.js');
-shell.echo('Let the drive sequence being, hit it Pinback.');
+shell.echo('Let the drive sequence begin, hit it Pinback.');
 shell.exec('cd node_modules/enclave && npm start');
