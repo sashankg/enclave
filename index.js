327e3fc68f81b764bf80ac494e9fb0852449d519
ean <eanplatter@gmail.com>
2016-03-03 10:53:16 -0600

    paths

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var shell = require('shelljs');
-shell.cat('../../.enclaverc')
+shell.cat('./.enclaverc')
 shell.echo('Let the drive sequence being, hit it Pinback.');
 shell.exec('cd node_modules/enclave && npm start');
