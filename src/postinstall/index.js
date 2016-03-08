83e08cf93914d062e8aba05c1650487e1ac0bb78
ean <eanplatter@gmail.com>
2016-03-08 12:33:44 -0600

    document

--- src/postinstall/index.js
+++ src/postinstall/index.js
@@ -3,23 +3,72 @@ var shell = require('shelljs')
 var prompt = require('prompt')
 var prompts = require('./prompts')
 var spinner = require('../cli-helpers/spinner')
-prompt.start()
+var clientFiles = require('./clientFiles')
+
+/**
+ * Handle errors from the cli prompts.
+ * @param err
+ * @returns {number}
+ */
 function onErr(err) {
   console.log(err)
   return 1
 }
-shell.exec('bash ./asciiart')
-prompt.get(prompts, function (err, result) {
-  shell.exec('rm ../../enclave.js')
-  shell.exec('touch ../../enclave.js')
+
+/**
+ * Runs the spinner for a given amount of time. Currently this isn't being used for any kind of asynchronous action
+ * but rather is there to improve the UX of the CLI too. After postinstall the terminal get's flooded with a list of
+ * install modules, which hides the postinstall instructions.
+ * @param {number} time - How long you want the spinner to run.
+ */
+function preventFinishFor(time) {
+  spinner.start()
+  setTimeout(function () {
+    spinner.stop()
+  }, time)
+}
+
+
+/**
+ * An object of strings used for inserting the start script into the user's package.json file with
+ * the `shell.sed` command.
+ * @type {{flag: string, insertionPoint: string, addition: string, file: string}}
+ */
+var insertScript = {
+  flag: '-i',
+  insertionPoint: '"scripts": {',
+  addition: "\"scripts\": { \n    \"start\": \"node node_modules/enclave/index.js\",",
+  file: clientFiles.package
+}
+
+
+/**
+ * Start the CLI prompt.
+ */
+prompt.start()
+
+/**
+ * Throw down that postinstall logo.
+ */
+shell.exec('bash ../cli-helpers/postinstall-logo')
+
+/**
+ * Clean out any currently existing config file for a fresh one.
+ */
+shell.exec('rm ' + clientFiles.config)
+shell.exec('touch ' + clientFiles.config)
+
+function configureConfigFile(err, result) {
   for (var key in result) {
     if (key === 'port' && !result[key] || key === 'port' && result[key] !== result[key]) {
-      shell.echo("exports." + key + " = 8080" + '\n').toEnd('../../enclave.js')
+      shell.echo("exports." + key + " = 8080" + '\n').toEnd(clientFiles.config)
     } else {
-      shell.echo("exports." + key + " = " + JSON.stringify(result[key]) + '\n').toEnd('../../enclave.js')
+      shell.echo("exports." + key + " = " + JSON.stringify(result[key]) + '\n').toEnd(clientFiles.config)
     }
   }
-  if (err) { return onErr(err) }
+  if (err) {
+    return onErr(err)
+  }
   console.log('Here\'s what I\'ve got down, if something is wrong you can edit this in your enclave.js file.:'.yellow)
   console.log('  entry: '.red + result.entry.magenta)
   console.log('  output: '.red + result.output.magenta)
@@ -27,10 +76,8 @@ prompt.get(prompts, function (err, result) {
   console.log('  index: '.red + result.index.magenta)
   console.log('  live: '.red + result.live)
   console.log('To run your app, just type'.green, '$ npm start'.bold.green)
-  var newScript = "\"scripts\": { \n    \"start\": \"node node_modules/enclave/index.js\","
-  spinner.start();
-  setTimeout(() => {
-    spinner.stop()
-  }, 5000)
-  shell.sed('-i', '"scripts": {', newScript, '../../package.json');
-})
+  shell.sed(insertScript.flag, insertScript.insertionPoint, insertScript.addition, insertScript.file)
+  preventFinishFor(5000)
+}
+
+prompt.get(prompts, configureConfigFile(err, result))
