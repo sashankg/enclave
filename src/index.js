06368a881c4054063bbd2ab6467e1ad4fc1c4c76
ean <eanplatter@gmail.com>
2016-03-08 14:29:36 -0600

    fix path issue

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 var shell = require('shelljs')
-var userSettings = require('../../enclave.js')
+var userSettings = require('../../../enclave.js')
 shell.echo('Let the drive sequence begin, hit it Pinback.')
 shell.exec('cd node_modules/enclave && webpack && webpack-dev-server --port ' + JSON.parse(userSettings.port))
